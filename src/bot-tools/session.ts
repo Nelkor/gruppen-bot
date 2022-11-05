7bfbbeceb639869be7ec5c86e4112e40af709c90
nelkor <a.tutunnik@corp.mail.ru>
2022-11-05 17:37:36 +0600

    session key

--- src/bot-tools/session.ts
+++ src/bot-tools/session.ts
@@ -1,5 +1,10 @@
+import { Context } from 'grammy'
+
 import { SessionData } from './types'
 
 export const initial = (): SessionData => ({
   pizzaCount: 0,
 })
+
+export const getSessionKey = ({ from, chat }: Context) =>
+  !from || !chat ? undefined : `${from.id}/${chat.id}`
--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,23 @@ import { connect, connection } from 'mongoose'
 import { MongoDBAdapter, type ISession } from '@grammyjs/storage-mongodb'
 import { Bot, session } from 'grammy'
 
-import { token, mongoUri, launchBot, initial, GruppenBot } from '@/bot-tools'
+import {
+  token,
+  mongoUri,
+  launchBot,
+  initial,
+  getSessionKey,
+  GruppenBot,
+} from '@/bot-tools'
 
 import { setHandlers } from './handlers'
 
 connect(mongoUri).then(() => {
   const collection = connection.db.collection<ISession>('sessions')
+  const storage = new MongoDBAdapter({ collection })
   const bot: GruppenBot = new Bot(token)
 
-  bot.use(
-    session({
-      initial,
-      storage: new MongoDBAdapter({ collection }),
-    })
-  )
+  bot.use(session({ initial, getSessionKey, storage }))
 
   setHandlers(bot)
   launchBot(bot)
